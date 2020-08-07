9f6cffb273d6b9d4335ecb30e6c3841df4692e2c
NaiWuPu <15542848579@163.com>
2020-08-07 10:40:28 +0800

    文件上传和下载

--- evaler/ex1/main.go
+++ evaler/ex1/main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/soniah/evaler"
 )
 
+var In = make(chan int64, 50)
+
 func main() {
 	result, _ := evaler.Eval("(1+2.2+4.1*2)/3-0.31")
 	res := evaler.BigratToFloat(result)
@@ -17,4 +19,16 @@ func main() {
 	fmt.Printf("运算结果2为:%v \n", res)
 	fmt.Printf("运算结果3为:%v \n", res1)
 	fmt.Printf("运算结果4为:%v \n", res2)
+
+	go func() {
+		for a := 0;; a++ {
+			In <- 1
+		}
+	}()
+	go func() {
+		for {
+			fmt.Println(<- In)
+		}
+	}()
+	select {}
 }
